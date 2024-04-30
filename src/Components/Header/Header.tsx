400cfc636ea7e71014186a04e6d8d73f17a56b8c
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-04-30 00:16:34 -0400

    style: header styles

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,13 +1,16 @@
 export const Header = () => {
   return (
     <>
-      <header>
-        <h1>Social Media</h1>
-        <p>Total Followers: 23,004</p>
-        <hr />
-
+      <header className="bg-very-pale-blue h-[235px] rounded-b-[20px] pt-8 px-6">
+        <h1 className="text-very-dark-blue text-2xl font-bold mb-1">
+          Social Media Dashboard
+        </h1>
+        <p className="text-dark-grayish-blue font-semibold mb-6">
+          Total Followers: 23,004
+        </p>
+        <hr className="bg-black mb-5" />
         <div className="flex justify-between">
-          <p>Dark Mode</p>
+          <p className="text-dark-grayish-blue font-semibold">Dark Mode</p>
           <input type="checkbox" name="darkmode" id="darkmode" />
         </div>
       </header>
